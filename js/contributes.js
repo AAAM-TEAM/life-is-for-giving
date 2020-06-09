7d34604c608907883c342f60074739823953a144
Student <mm.nemrawi96@gmail.com>
2020-06-09 12:32:24 +0300

    add remove button for conributes page

--- js/contributes.js
+++ js/contributes.js
@@ -10,18 +10,22 @@ function renderDataFromLocalStorage() {
     getMain.appendChild(div1);
     var imageContainer = document.createElement("img");
     imageContainer.src = `img/${Donate.allDonates[x].itemName}.png`;
-    getMain.appendChild(imageContainer);
+    div1.appendChild(imageContainer);
     console.log(imageContainer.src);
     var paragraph1 = document.createElement("p");
     paragraph1.textContent = `Quantity: ${Donate.allDonates[x].numberQuantity}`;
-    getMain.appendChild(paragraph1);
+    div1.appendChild(paragraph1);
     var paragraph2 = document.createElement("p");
     paragraph2.textContent = `quality: ${Donate.allDonates[x].qualityType}`;
-    getMain.appendChild(paragraph2);
+    div1.appendChild(paragraph2);
     var paragraph3 = document.createElement("p");
     paragraph3.textContent = `city Name: ${Donate.allDonates[x].cityName}`;
-    getMain.appendChild(paragraph3);
+    div1.appendChild(paragraph3);
     var contributesTitle = document.getElementById('contributeTitle3');
+    var removeButton = document.createElement('button');
+    removeButton.setAttribute('id', x);
+    div1.appendChild(removeButton);
+    removeButton.textContent = "x";
     if (localStorage) {
       document.getElementById("removemessage").innerHTML = `<section id="contributes_befor-submit3">
       <!-- section if there weren't any contributes -->
@@ -39,3 +43,17 @@ function renderDataFromLocalStorage() {
 }
 console.log("leng", Donate.allDonates.length);
 renderDataFromLocalStorage();
+function removeData () {
+  getMain.innerHTML = '';
+}
+getMain.addEventListener('click', (event)=>{
+  console.log(event.target);
+  var deleteId; 
+  if (event.target.textContent === 'x'){
+    deleteId = event.target.id;
+    Donate.allDonates.splice(deleteId,1);
+    sendToLocalStorage();
+    removeData();
+    renderDataFromLocalStorage();
+  }
+});
